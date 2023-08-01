bc691d09bec51ccd6de98782810192581430a64c
Gil Moura <gilmoura82@gmail.com>
2023-08-01 07:18:12 -0300

    Criando as interfaces Vendedor e Departamento

---
+++ src/model/dao/DepartamentoDAO.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Departamento;
+
+public interface DepartamentoDAO {
+
+	void insert(Departamento obj);
+	void update (Departamento obj);
+	void deleteById(Integer id);
+	Departamento findById(Integer id);
+	List<Departamento> findAll();
+}
